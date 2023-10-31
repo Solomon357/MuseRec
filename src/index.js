1500c5409607d9a93fe6e6970dcbda53fff31a05
Solomon357 <Solomonoddy@hotmail.com>
2023-10-31 19:37:03 +0000

    playing with tests and adding custom icon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
-import './index.css';
 import App from './App';
 import { radioTheme } from './customthemes/radioStyle';
 import { buttonTheme } from './customthemes/buttonStyle';
